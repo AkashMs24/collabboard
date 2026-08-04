7e55ab76d8e3d7b46436a22c441d927f3b2b7fc7
AKASH M S <manigarakash@gmail.com>
2026-08-04 10:04:27 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,16 @@
+// playwright.config.js
 module.exports = {
   testDir: './e2e',
   timeout: 30000,
   retries: 1,
+  fullyParallel: false,
+  workers: 1,
   use: {
     headless: true,
     viewport: { width: 1280, height: 800 },
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
+    trace: 'retain-on-failure',
   },
+  reporter: [['html', { open: 'never' }], ['list']],
 };
